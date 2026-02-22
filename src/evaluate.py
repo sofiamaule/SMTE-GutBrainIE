45963b64a128fe9c3db7f61f008a48b8994fbdb7
sofiamaule <sofiamaule16@gmail.com>
2026-02-22 19:37:07 +0100

    bert baseline

--- src/evaluate.py
+++ src/evaluate.py
@@ -3,7 +3,7 @@ import json
 
 # DEFINE HERE THE PATH(S) TO YOUR PREDICTIONS
 PREDICTIONS_PATH_NER = ('C:/Users/super/Documents/UniPd/ATA/GutBrainIE/src/ner/predictions/bert_RE_dev_thr_0.65_eval_format.json')
-PREDICTIONS_PATH_MENTION_LEVEL_RE ='C:/Users/super/Documents/UniPd/ATA/GutBrainIE/src/re/predictions/bert_RE__thr_0.65_eval_format.json'
+PREDICTIONS_PATH_MENTION_LEVEL_RE ='C:/Users/super/Documents/UniPd/ATA/GutBrainIE/src/re/predictions/bert_re.json'
 PREDICTIONS_PATH_CONCEPT_LEVEL_RE = 'teamID_CONCEPT_LEVEL_RE_runID_systemDesc.json'
 
 # DEFINE HERE FOR WHICH SUBTASK(S) YOU WANT TO EVAL YOUR PREDICTIONS
