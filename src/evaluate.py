6a0cad3e7d1f8be2de68a4a1891f3620036689b5
sofiamaule <sofiamaule16@gmail.com>
2026-03-18 15:28:06 +0100

    ESEMBLE

--- src/evaluate.py
+++ src/evaluate.py
@@ -2,7 +2,7 @@
 import json
 
 # DEFINE HERE THE PATH(S) TO YOUR PREDICTIONS
-PREDICTIONS_PATH_NER = ('C:/Users/super/Documents/UniPd/ATA/GutBrainIE/src/ner/predictions/pred_NB4_annotator_weights_bronze.json')
+PREDICTIONS_PATH_NER = ('C:/Users/super/Documents/UniPd/ATA/GutBrainIE/src/ner/predictions/pred_ensemble_hybrid_all.json')
 PREDICTIONS_PATH_MENTION_LEVEL_RE ='C:/Users/super/Documents/UniPd/ATA/GutBrainIE/src/re/predictions/pred_NB4_annotator_weights_bronze.json'
 PREDICTIONS_PATH_CONCEPT_LEVEL_RE = 'teamID_CONCEPT_LEVEL_RE_runID_systemDesc.json'
 
